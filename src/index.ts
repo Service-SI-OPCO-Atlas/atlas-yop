0023fa1e98a46387da49c0f50645e07b9d28006d
Franck Wolff <frawolff@gmail.com>
2023-03-31 10:09:27 +0200

    Export all shemas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import Yop from "./yop/Yop";
 
 export default Yop;
+
+export { AnySchema } from './yop/AnySchema';
+export { ArraySchema } from './yop/ArraySchema';
+export { BooleanSchema } from './yop/BooleanSchema';
+export { DateSchema } from './yop/DateSchema';
+export { FileSchema } from './yop/FileSchema';
+export { IgnoredSchema } from './yop/IgnoredSchema';
+export { NumberSchema } from './yop/NumberSchema';
+export { ObjectSchema } from './yop/ObjectSchema';
+export { StringSchema } from './yop/StringSchema';

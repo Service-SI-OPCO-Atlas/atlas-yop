827c6857968062bd1789e36c3ba35f8223b47291
Franck Wolff <frawolff@gmail.com>
2023-04-07 16:45:35 +0200

    Only minify code for release

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'tsup'
 
+const env = process.env.NODE_ENV
+
 export default defineConfig({
     entry: ['src/index.ts'],
     dts: true,
     sourcemap: true,
     clean: true,
-    minify: true,
+    minify: env === 'release',
     target: "es6",
     format: ['esm'],
     outExtension() {

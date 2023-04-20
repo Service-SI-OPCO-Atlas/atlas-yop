c3a32a56e4c2938885367c83cf2789f9cdf542f9
Franck Wolff <frawolff@gmail.com>
2023-04-20 18:55:51 +0200

    Export SchemaType and ObjectPropertiesSchemas

--- src/index.ts
+++ src/index.ts
@@ -28,5 +28,5 @@ export { DateSchema } from './yop/DateSchema';
 export { FileSchema } from './yop/FileSchema';
 export { IgnoredSchema } from './yop/IgnoredSchema';
 export { NumberSchema } from './yop/NumberSchema';
-export { ObjectSchema } from './yop/ObjectSchema';
+export { SchemaType, ObjectPropertiesSchemas, ObjectSchema } from './yop/ObjectSchema';
 export { StringSchema } from './yop/StringSchema';
